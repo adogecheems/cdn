9c1574ca4cbd6607f3b2ea77b9ab143242e10522
adogecheems <adogecheems@outlook.com>
2025-07-04 18:19:04 +0800

    ...

--- source/blog/webp.sh
+++ source/blog/webp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find . -type f -iname "*.png" | while read -r file; do
-  webp_file="${file%.png}.webp"
+find . -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) | while read -r file; do
+  webp_file="${file%.*}.webp"
   cwebp "$file" -o "$webp_file"
   echo "Converted $file -> $webp_file (quality $QUALITY)"
 done
